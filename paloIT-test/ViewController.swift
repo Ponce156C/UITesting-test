94af2cdf4f02e3fe42e10fc8a16464c270c969b8
Carlos Ponce <carlosponce156@gmail.com>
2020-07-29 15:58:23 -0500

    Initial Commit

---
+++ paloIT-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  paloIT-test
+//
+//  Created by Carlos Ponce on 29/07/20.
+//  Copyright © 2020 Carlos Ponce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
